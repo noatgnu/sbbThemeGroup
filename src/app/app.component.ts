b2f625d81c8851ca9cac25899875ce723db50480
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2018-12-03 10:18:36 +1000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Structural and Computational Biology Theme Group';
+  title = 'Structural Biology and Biochemistry Theme Group';
   collapsed = true;
 
   toggleCollapsed(): void {
